cace1a73de3143b1e643bd80253fa375e78e1626
Dalton <daltonkajander@knights.ucf.edu>
2022-12-11 16:39:12 -0500

    D3P1
    Python

--- 2022/Day 3 - Rucksack Organization/part1.py
+++ 2022/Day 3 - Rucksack Organization/part1.py
@@ -1,1 +1,34 @@
-filename = "test"
+filename = "input"
+
+def getPriorityOfChar(c):
+	if (c == c.lower()):
+		return(ord(c) - 96)
+	return(ord(c) - 38)
+		
+def findError(rucksack):
+	compSize = len(rucksack) // 2
+	comp1, comp2 = rucksack[:compSize], rucksack[compSize:]
+	assert(len(comp1) == len(comp2))
+	
+	err = ''
+	for c in comp1:
+		if c in comp2:
+			err = c
+			break
+	
+	
+	return getPriorityOfChar(err)
+
+
+txt = ""
+with open(filename + ".txt", "r") as file:
+	txt = file.readlines()
+	for index,value in enumerate(txt):
+		txt[index] = txt[index][:-1]
+
+sum = 0
+for sack in txt:
+	sum += findError(sack)
+
+
+print(f"{sum=}")
